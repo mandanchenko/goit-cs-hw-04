88e19b87ff1e201e1c7de7fb55add89c5c55baec
Andanchenko Maria <andanchenko.maria@gmail.com>
2024-03-10 11:47:35 +0200

    Update task1.py

--- task1.py
+++ task1.py
@@ -3,23 +3,22 @@ from pathlib import Path
 from threading import Thread
 
 
-def search_keywords_in_file(keywords_list, file_name):
+def search_keywords_in_file(keywords_list, file_name, results):
     try:
         with open(file_name, "r", encoding="utf-8") as f:
             lines = f.readlines()
-            for line_number, line in enumerate(lines, 1):
-                for keyword in keywords_list:
+            for keyword in keywords_list:
+                for line in lines:
                     if keyword in line:
-                        print(
-                            f"Знайдено '{keyword}' у файлі '{file_name}' на рядку {line_number}"
-                        )
-
+                        results[keyword].append(file_name)
+                        break
     except FileNotFoundError:
         print(f"Файл '{file_name}' не знайдено.")
     except PermissionError as pe:
         print(f"Відмовлено у доступі до файлу '{file_name}': {pe}")
     except Exception as e:
         print(f"Сталася помилка під час обробки файлу '{file_name}': {e}")
+    return results
 
 
 def get_files(path: Path):
@@ -32,10 +31,12 @@ def get_files(path: Path):
     return input_files
 
 
-if __name__ == "__main__":
+def main():
     source = Path(input("Введіть шлях до директорії: "))
     keywords_input = input("Введіть ключові слова, розділені пробілом: ")
     keywords = keywords_input.split()
+
+    results = {keyword: [] for keyword in keywords}
 
     format = "%(threadName)s %(asctime)s: %(message)s"
     logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
@@ -45,11 +46,21 @@ if __name__ == "__main__":
 
     threads = []
     for file in files:
-        thread = Thread(target=search_keywords_in_file, args=(keywords, file))
+        thread = Thread(target=search_keywords_in_file, args=(keywords, file, results))
         threads.append(thread)
         thread.start()
 
     for thread in threads:
         thread.join()
 
+    # Виведення результатів пошуку
+    for keyword, matches in results.items():
+        print(f"Результати для ключового слова '{keyword}':")
+        for match in matches:
+            print(f"  Знайдено у файлі '{match}")
+
     print("Всі файли перевірені.")
+
+
+if __name__ == "__main__":
+    main()
